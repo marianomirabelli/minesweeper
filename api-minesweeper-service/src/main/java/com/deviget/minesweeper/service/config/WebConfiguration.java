860923c97232b0ffbd824ec2226a04f8e755b731
Mariano <mariano2106@gmail.com>
2020-11-20 13:50:52 -0300

    chore: jetty mapper configuration

---
+++ api-minesweeper-service/src/main/java/com/deviget/minesweeper/service/config/WebConfiguration.java
@@ -0,0 +1,45 @@
+package com.deviget.minesweeper.service.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.boot.actuate.info.InfoEndpoint;
+import org.springframework.boot.actuate.info.MapInfoContributor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.yaml.snakeyaml.Yaml;
+
+import java.util.Arrays;
+import java.util.Map;
+
+@Configuration
+public class WebConfiguration {
+
+    private final static String APP_INFO = "app-info.yml";
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2CborHttpMessageConverter(ObjectMapper objectMapper) {
+        return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public InfoEndpoint infoEndpoint() {
+        Yaml yaml = new Yaml();
+        Map<String, Object> appInfo = (Map<String, Object>) yaml
+                .loadAs(this.getClass().getClassLoader().getResourceAsStream(APP_INFO), Map.class);
+        InfoContributor infoContributor = new MapInfoContributor(appInfo);
+        return new InfoEndpoint(Arrays.asList(infoContributor));
+    }
+}
