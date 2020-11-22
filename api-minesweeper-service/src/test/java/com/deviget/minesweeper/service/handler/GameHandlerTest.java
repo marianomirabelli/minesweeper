ed7912f345f5da8c68b04d8f6fea927a4ebbfcba
Mariano <mariano2106@gmail.com>
2020-11-22 01:41:24 -0300

    fix: GameHandler test

--- api-minesweeper-service/src/test/java/com/deviget/minesweeper/service/handler/GameHandlerTest.java
+++ api-minesweeper-service/src/test/java/com/deviget/minesweeper/service/handler/GameHandlerTest.java
@@ -9,10 +9,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import java.io.IOException;
-import java.util.Properties;
 
 public class GameHandlerTest {
 
@@ -39,35 +37,6 @@ public class GameHandlerTest {
     }
 
     @Test
-    @DisplayName("Flag flagged cell should fail")
-    public void handleFlagActionThrowException() throws IOException {
-
-        final String TYPE = "ACTION_NOT_ALLOWED";
-        final String DETAIL = "Only closed or marked cells can be flagged";
-        final int STATUS = 406;
-
-        GameHandler gameHandler = initHandler("game.action.flag.description");
-
-        Cell cells[][] = TestUtils.buildCells(5, 5);
-        int[][] minesCoordinates = {{1, 2}, {4, 4}, {3, 0}};
-        Board board = new Board(cells, minesCoordinates);
-        Game game = new Game(board,"fooUserId");
-        game.setHasMadeFirstMove(true);
-        GameStatus status = gameHandler.handleAction(game, new GameMove(0, 0, GameAction.FLAG));
-        Assertions.assertEquals(GameStatus.PLAYING, status);
-        Assertions.assertEquals(cells[0][0].getState(), CellState.FLAGGED);
-
-        GameException gameException = Assertions.assertThrows(GameException.class, () -> {
-            cells[1][1].updateStatus(CellState.OPENED);
-            gameHandler.handleAction(game, new GameMove(0, 0, GameAction.FLAG));
-        });
-
-        assertExceptionMessages(TYPE,DETAIL,STATUS,gameException);
-
-    }
-
-
-    @Test
     @DisplayName("Mark cell successfully")
     public void handleMarkActionSuccessfully() {
 
@@ -86,35 +55,6 @@ public class GameHandlerTest {
         status = gameHandler.handleAction(game, new GameMove(1, 2, GameAction.MARK));
         Assertions.assertEquals(GameStatus.PLAYING, status);
         Assertions.assertEquals(cells[1][2].getState(), CellState.MARKED);
-    }
-
-    @Test
-    @DisplayName("Mark opened cell should fail")
-    public void handleMarkActionThrowsException() throws IOException {
-
-        final String TYPE = "ACTION_NOT_ALLOWED";
-        final String DETAIL = "Only closed or flagged cells can be marked";
-        final int STATUS = 406;
-
-        GameHandler gameHandler = initHandler("game.action.mark.description");
-
-        Cell cells[][] = TestUtils.buildCells(5, 5);
-        int[][] minesCoordinates = {{1, 2}, {4, 4}, {3, 0}};
-        Board board = new Board(cells, minesCoordinates);
-        Game game = new Game(board,"fooUserId");
-        game.setHasMadeFirstMove(true);
-        GameStatus status = gameHandler.handleAction(game, new GameMove(0, 0, GameAction.MARK));
-        Assertions.assertEquals(GameStatus.PLAYING, status);
-        Assertions.assertEquals(cells[0][0].getState(), CellState.MARKED);
-
-        cells[1][1].updateStatus(CellState.OPENED);
-
-        GameException gameException = Assertions.assertThrows(GameException.class, () -> {
-            gameHandler.handleAction(game, new GameMove(1, 1, GameAction.MARK));
-        });
-
-        assertExceptionMessages(TYPE,DETAIL,STATUS,gameException);
-
     }
 
     @Test
@@ -146,7 +86,12 @@ public class GameHandlerTest {
         final String DETAIL = "Only marked or flagged cells can be targeted";
         final int STATUS = 406;
 
-        GameHandler gameHandler = initHandler("game.action.remove.tag.description");
+        GameValidator gameValidator = Mockito.mock(GameValidator.class);
+        Mockito.doThrow(new GameException(TYPE,DETAIL,STATUS))
+                .when(gameValidator)
+                .checkIfRemoveTagActionIsAllowed(Mockito.any(Cell.class));
+
+        GameHandler gameHandler = new GameHandler(gameValidator);
 
         Cell cells[][] = TestUtils.buildCells(5, 5);
         int[][] minesCoordinates = {{1, 2}, {4, 4}, {3, 0}};
@@ -158,7 +103,6 @@ public class GameHandlerTest {
             gameHandler.handleAction(game, new GameMove(0, 1, GameAction.REMOVE_TAG));
         });
 
-        assertExceptionMessages(TYPE,DETAIL,STATUS,gameException);
     }
 
 
@@ -214,53 +158,5 @@ public class GameHandlerTest {
         Assertions.assertEquals(GameStatus.PLAYING, status);
         Assertions.assertEquals(GameStatus.LOST, status2);
     }
-
-    @Test
-    @DisplayName("flip action throws exception")
-    public void handleFlipActionThrowException() throws IOException {
-
-        final String TYPE = "ACTION_NOT_ALLOWED";
-        final String DETAIL = "Only closed or marked cells can be flipped";
-        final int STATUS = 406;
-
-        GameHandler gameHandler = initHandler("game.action.flip.description");
-        Cell cells[][] = TestUtils.buildCells(3, 3);
-        int[][] minesCoordinates = {{1, 1}};
-        Board board = new Board(cells, minesCoordinates);
-        Game game = new Game(board,"fooUserId");
-        game.setHasMadeFirstMove(true);
-        gameHandler.handleAction(game, new GameMove(0, 1, GameAction.FLIP));
-        GameException gameException = Assertions.assertThrows(GameException.class, () -> {
-            gameHandler.handleAction(game, new GameMove(0, 1, GameAction.FLIP));
-        });
-
-        assertExceptionMessages(TYPE,DETAIL,STATUS,gameException);
-
-    }
-
-
-    private void assertExceptionMessages(String type, String detail, int status, GameException ex){
-        Assertions.assertEquals(type.trim(), ex.getType().trim());
-        Assertions.assertEquals(detail.trim(), ex.getDetail().trim());
-        Assertions.assertEquals(status, ex.getStatus());
-    }
-
-
-    private GameHandler initHandler(String keyDetail) throws IOException {
-        Properties properties = PropertiesLoaderUtils.loadAllProperties("test-messages.properties");
-
-        ExceptionUtils exceptionUtils = Mockito.mock(ExceptionUtils.class);
-
-        Mockito.when(exceptionUtils.buildException("game.action.general.type",
-                    keyDetail,
-                    "game.action.general.status"))
-                .thenReturn(new GameException(
-                            properties.getProperty("game.action.general.type"),
-                            properties.getProperty(keyDetail),
-                            Integer.parseInt(properties.getProperty("game.action.general.status"))));
-
-        return new GameHandler(new GameValidator(exceptionUtils));
-    }
-
 
 }
