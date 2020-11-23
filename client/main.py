213841b26c7541afe0571150ec56e2cc4d29cd4c
Mariano <mariano2106@gmail.com>
2020-11-23 10:55:02 -0300

    feat: print board at client

--- client/main.py
+++ client/main.py
@@ -2,8 +2,22 @@ from MinesweeperClient import MinesweeperConnector
 import random
 import string
 
+def get_image(cell):
+
+    dictionary = {"MINE": "*", "NUMBERED": str(cell['minesAround']), "BLANK": "B", "CLOSED": 'C'}
+    return dictionary[cell['state']]
+
+def print_board(board):
+
+    print('\n'.join([''.join(['{:4}'.format(get_image(item)) for item in row])
+                     for row in board]))
+
+
 if __name__ == "__main__":
 
+    rows = 10
+    columns = 10
+    mines = 15
     print("Creating Random  user")
     letters = string.ascii_lowercase
 
@@ -14,9 +28,9 @@ if __name__ == "__main__":
     userResponse = minesweeper_connector.create_user(user)
 
     newGame = {
-        "rows": 5,
-        "columns": 5,
-        "mines": 5
+        "rows": rows,
+        "columns": columns,
+        "mines": mines
     }
 
     print("Creating new Game")
@@ -27,16 +41,19 @@ if __name__ == "__main__":
         try:
 
             game_move = {
-                "row":random.randint(0,4) ,
-                "column":random.randint(0,4),
+                "row":random.randint(0,rows-1) ,
+                "column":random.randint(0,columns-1),
                 "action": "FLIP"
             }
             print("Executing FLIP action at cell %s %s" % (game_move['row'], game_move['column']))
             response = minesweeper_connector.play_game(id, game_move)
+            print_board(response['board']['cells'])
             game_status = response['status']
         except Exception as e:
             print('The FLIP action has failed at cell %s %s due to %s'%(game_move['row'],game_move['column'],e))
 
     game_response = minesweeper_connector.get_game(game_response['id'])
     print('The user "%s has %s the game id %s ' %(user['userName'],game_status,game_response['id']))
+
+
 
